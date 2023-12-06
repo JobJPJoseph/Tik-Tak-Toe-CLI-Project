f12795b5fa877d237acdc7da90fd697a042b4c09
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2023-12-06 01:34:39 -0500

    We set up the cpu class and test specs and got it working.

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Our first task is to create a player.js file and spec file to go with it. We nee
 We need to create a player class that accepts an object that represents the 'readline' object.
 The class will have two instance methods: getCoordinate and validateCoordinate.
 Also need to create tests for them.
+
+## Phase 2
+Our next task is to create a cpu class that emulates the player class. This time the cpu will grab a random coordinate from the availableCoordinates and return it. No need to validate it or format it.
---
+++ Tik-Tak-Toe-Project/tests/cpu.spec.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+const Cpu = require('../lib/cpu');
+
+describe('Random CPU Coordinate', function () {
+    describe('Get Coordinate', function () {
+        it('it should get a random coordinate from the available coordinates', function () {
+            const cpu = new Cpu();
+            const availableCoordinates = [
+                [0,0], [0,1], [0,2],
+                [1,0], [1,1], [1,2],
+                [2,0], [2,1], [2,2],
+            ];
+            const cpuCoordinate = cpu.getCoordinate(availableCoordinates);
+            const result = availableCoordinates.includes(cpuCoordinate);
+            expect(result).to.equal(true);
+        })
+    })
+})
